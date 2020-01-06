7d1826428a66cda349996c42c6e57a9d9ab8bea7
Monlih <MonliH@users.noreply.github.com>
2020-01-06 06:50:14 -0500

    Fixed rooms

--- ChatApp/src/main/resources/Static/js/pages/rooms.js
+++ ChatApp/src/main/resources/Static/js/pages/rooms.js
@@ -5,8 +5,8 @@ import React from "react";
 export class Rooms extends React.Component {
   constructor(props) {
     super(props);
-    this.token = this.props.token;
-    console.log(this.props); // note: https://stackoverflow.com/questions/52064303/reactjs-pass-props-with-redirect-component
+    this.user = this.props.location.state.user;
+    console.log(this.user)
   }
   render() {
     return <h1>Rooms</h1>
